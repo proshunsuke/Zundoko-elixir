eb770f46acfc4bb6424237c6b6beed7cff4ad731
proshunsuke <shunsuke0901@gmail.com>
2016-03-16 23:59:59 +0900

    とりあえずズンドコ

--- zundoko.ex
+++ zundoko.ex
@@ -1,1 +1,18 @@
-IO.inspect "aa"
+defmodule Zundoko do
+	def kiyoshi(['ズン', 'ズン', 'ズン', 'ズン', 'ドコ']) do
+		IO.puts 'キヨシ!'
+	end
+
+	def kiyoshi(list) do
+		word = Enum.random(['ズン', 'ドコ'])
+		IO.write word
+		added_list = list ++ [word]
+		if length(added_list) >= 6 do
+			kiyoshi(List.delete_at(added_list, 0))
+		else
+			kiyoshi(added_list)
+		end
+	end
+end
+
+Zundoko.kiyoshi([])
